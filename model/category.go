84396e903806f8400e253969a3bad75f01da4ffa
Kehinde ODETOLA <odetolakehinde5@gmail.com>
2023-04-25 22:32:43 +0100

    add image struct to categories

--- model/category.go
+++ model/category.go
@@ -20,16 +20,25 @@ type (
 
 	// Category schema
 	Category struct {
-		ID          int           `mapstructure:"id" json:"id"`
-		Name        string        `mapstructure:"name" json:"name"`
-		Slug        string        `mapstructure:"slug" json:"slug"`
-		Parent      int           `mapstructure:"parent" json:"parent"`
-		Description string        `mapstructure:"description" json:"description"`
-		Display     string        `mapstructure:"display" json:"display"`
-		Image       []interface{} `mapstructure:"image" json:"image"`
-		MenuOrder   int           `mapstructure:"menu_order" json:"menu_order"`
-		Count       int           `mapstructure:"count" json:"count"`
-		Links       struct {
+		ID          int    `mapstructure:"id" json:"id"`
+		Name        string `mapstructure:"name" json:"name"`
+		Slug        string `mapstructure:"slug" json:"slug"`
+		Parent      int    `mapstructure:"parent" json:"parent"`
+		Description string `mapstructure:"description" json:"description"`
+		Display     string `mapstructure:"display" json:"display"`
+		Image       []struct {
+			ID              int    `mapstructure:"id" json:"id"`
+			DateCreated     string `mapstructure:"date_created" json:"date_created"`
+			DateCreatedGmt  string `mapstructure:"date_created_gmt" json:"date_created_gmt"`
+			DateModified    string `mapstructure:"date_modified" json:"date_modified"`
+			DateModifiedGmt string `mapstructure:"date_modified_gmt" json:"date_modified_gmt"`
+			Src             string `mapstructure:"src" json:"src"`
+			Name            string `mapstructure:"name" json:"name"`
+			Alt             string `mapstructure:"alt" json:"alt"`
+		} `mapstructure:"image,omitempty" json:"image,omitempty"`
+		MenuOrder int `mapstructure:"menu_order" json:"menu_order"`
+		Count     int `mapstructure:"count" json:"count"`
+		Links     struct {
 			Self []struct {
 				Href string `mapstructure:"href" json:"href"`
 			} `mapstructure:"self" json:"self"`
